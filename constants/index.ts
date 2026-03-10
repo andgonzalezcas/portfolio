3d3866589f0fe286a3b143203f1c9b93a6dea90c
Camilo Gonzalez <andgonzalezcas@unal.edu.co>
2026-03-10 02:49:59 -0500

    feat(app): add projects sections

--- constants/index.ts
+++ constants/index.ts
@@ -2,4 +2,10 @@ export const EXTERNAL_LINKS = {
     LINKEDIN: "https://www.linkedin.com/in/camilo-andres-gonzalez-castro-1a2515237/",
 };
 
-
+export const PROJECT_LINKS = [
+    "https://play.google.com/store/apps/details?id=me.viio.wallet.app&hl=es_CO",
+    "https://www.thehubdao.xyz/",
+    "https://viio.me/",
+    "https://github.com/andgonzalezcas/b12-challenge",
+    "https://github.com/andgonzalezcas/go_emergency_system"
+];
